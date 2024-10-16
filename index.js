d6c774a989274dd76ff7f5668b1195edd560c49c
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-10-16 21:40:28 +1000

    Got items rendering to page
    Items rendering to page. Some styling still to be done .
    Specifically on the ingredients and the add button.
    
    Funcitonality of add button still needs to be done
    
    Border-bottom
    
    margin-top

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 import {menuArray} from './data.js';
+
+
+function getMenuArray() {
+    const menuString = menuArray.map((item) => {
+        return (`
+        <div class="menu-item">
+            <div class="menu-item-description-left">
+                <p class="item-img">${item.emoji}</p>
+                <div class="menu-item-description">
+                    <p>${item.name}</p>
+                    <p>${item.ingredients.join(", ")}</p>
+                    <p>$${item.price}</p>
+                </div>
+            </div>
+            <button class="add-btn" id="${item.id}">+</button>
+        </div>
+            `)
+    }).join("");
+    return menuString;
+}
+
+function render() {
+    document.getElementById('menu').innerHTML = getMenuArray();
+}
+
+render();
